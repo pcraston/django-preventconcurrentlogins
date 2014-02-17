5eda2e9b0e8631ee3db86923d9db3300e873659a
Patrick Craston <patrick.craston@contextis.co.uk>
2014-02-17 08:57:47 +0000

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-preventconcurrentlogins',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django'],
@@ -22,7 +22,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
